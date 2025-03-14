21dc3f78d4a7b8f3be8a2926362817715e5e403c
simon-lennon <142030358+simon-lennon@users.noreply.github.com>
2025-03-14 15:48:41 +0000

    Add ESLint configuration file

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // Add any custom rules here
+    'vue/multi-word-component-names': 'off' // This allows single-word component names
+  }
+}
