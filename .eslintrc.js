93879c5b4587d6651fd587ec5c297e3e03add1be
simon-lennon <142030358+simon-lennon@users.noreply.github.com>
2025-03-14 15:49:01 +0000

    Update ESLint rules to be more lenient for demo

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,9 +13,10 @@ module.exports = {
     requireConfigFile: false
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    // Add any custom rules here
-    'vue/multi-word-component-names': 'off' // This allows single-word component names
+    'no-console': 'off', // Allow console for demo purposes
+    'no-debugger': 'off', // Allow debugger for demo purposes
+    'vue/multi-word-component-names': 'off', // Allow single-word component names
+    'no-unused-vars': 'warn', // Just warn on unused variables instead of error
+    'no-undef': 'warn' // Just warn on undefined variables
   }
 }
