890ac700203c80f847a34072d489e6232de4fe87
simon-lennon <142030358+simon-lennon@users.noreply.github.com>
2025-03-14 16:42:26 +0000

    Enable hot module replacement for development server

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true
-      }
+    hot: true,
+    liveReload: true,
+    client: {
+      overlay: true,
+      progress: true
     }
   }
 }
